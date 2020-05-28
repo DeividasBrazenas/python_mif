08fd9f9f2f019a819265bea773ff3ae0cfd285ec
Deividas Brazenas <deividas.brazenas@gmail.com>
2020-05-28 16:01:32 +0300

    Updating 1st excercise

--- PythonExercises/1/1.py
+++ PythonExercises/1/1.py
@@ -1,94 +1,114 @@
 import numpy as np
-from scipy.misc import derivative
-import scipy.integrate as integrate
-import sympy as sym
+import sympy as sp
+
 
 # 1. Padalinkite intervalą nuo -1.3 iki 2.5 tolygiai į 64 dalis.
-interval = np.linspace(-1.3, 2.5, 64)
+def n1_interval():
+    return np.linspace(-1.3, 2.5, 64)
 
-print(interval)
+
+print(n1_interval())
+
 
 # 2. Sugeneruokite masyvą dydžio 3n ir užpildykite jį cikliniu šablonu [1, 2, 3].
-n = 5;
-array = np.tile(np.array([1, 2, 3]), 3 * n)
+def n2_array(n):
+    return np.tile([1, 2, 3], 3 * n)
 
-print(array)
+
+print(n2_array(5))
+
 
 # 3. Sukurkite masyvą iš pirmųjų 10 nelyginių sveikųjų skaičių.
-array = np.arange(1, 21, 2)
+def n3_numbers():
+    return np.fromfunction(lambda n: (n * 2) + 1, (10,), dtype=int)
 
-print(array)
+
+print(n3_numbers())
+
 
 # 4. Sukurkite masyvą dydžio 10 x 10 iš nulių ir "įrėminkite" jį vienetais.
-def pad_with(vector, iaxis_pad_width, iaxis, kwargs):
-    pad_value = kwargs.get('padder', 1)
-    vector[:iaxis_pad_width[0]] = pad_value
-    vector[-iaxis_pad_width[1]:] = pad_value
-    return vector
+def n4_padded_array():
+    arr = np.zeros((10, 10), dtype=int)
+    arr = np.pad(arr, 1, mode="constant", constant_values=(1,))
+    return arr
 
-matrix = np.pad(np.zeros((8, 8)), 1, pad_with)
-print(matrix)
+
+print(n4_padded_array())
+
 
 # 5. Sukurkite masyvą dydžio 8 x 8, kur 1 ir 0 išdėlioti šachmatine tvarka (panaudokite slicing+striding metodą).
-matrix = np.zeros((8, 8))
-matrix[::2, ::2] = 1
-matrix[1::2, 1::2] = 1
+def n5_chess_matrix():
+    matrix = np.zeros((8, 8))
+    matrix[::2, ::2] = 1
+    matrix[1::2, 1::2] = 1
+    return matrix;
 
-print(matrix)
+
+print(n5_chess_matrix())
+
 
 # 6. Sukurkite masyvą dydžio n×n , kurio (i,j)-oji pozicija lygi i+j
-n = 3
-sums = np.zeros((3, 3))
-for idx, xval in enumerate(sums):
-    for idy, yval in enumerate(xval):
-        sums[idx, idy] = idx + idy
+def n6_matrix_sum(n):
+    matrix = np.zeros((3, 3))
+    for idx, xval in enumerate(matrix):
+        for idy, yval in enumerate(xval):
+            matrix[idx, idy] = idx + idy
 
-print(sums)
+
+print(n6_matrix_sum(3))
+
 
 # 7. Sukurkite atsitiktinį masyvą dydžio 3×5 naudodami np.random.rand(3, 5) funkciją ir suskaičiuokite: sumą, eilučių sumą, stulpelių sumą.
-matrix = np.random.rand(3, 5)
+def n7_random():
+    matrix = np.random.rand(3, 5)
+    return {"sum": np.sum(matrix), "row sum": matrix.sum(axis=1), "col sum": matrix.sum(axis=0)}
 
-print(np.sum(matrix))
-print(matrix.sum(axis=0))
-print(matrix.sum(axis=1))
+
+print(n7_random())
+
 
 # 8. Sukurkite atsitiktinį masyvą dydžio 5×5 naudodami np.random.rand(5, 5). Surūšiuokite eilutes pagal antrąjį stulpelį. Tam pamėginkite apjungti masyvo slicing + argsort + indexing metodus.
-matrix = np.random.rand(5, 5)
-print(matrix)
+def n8_matrix_sorted():
+    matrix = np.random.rand(5, 5)
+    indices = np.argsort(matrix[:, 1])
+    return matrix, matrix[indices]
 
-matrix = sorted(matrix, key=lambda col: col[1])
-print("########")
-print(np.reshape(matrix, (5, 5)))
+
+print(n8_matrix_sorted())
+
 
 # 9. Atvirkštinę matricą
-matrix = np.array([1, 2, 3, 4])
-matrix = np.reshape(matrix, (2, 2))
-matrix = np.linalg.inv(matrix)
+def n9_inverted_matrix():
+    matrix = np.array([[1, 2, 3], [-3, -2, -1], [4, 2, 3]])
+    return np.linalg.inv(matrix)
 
-print(matrix)
+
+print(n9_inverted_matrix())
+
 
 # 10. Apskaičiuokite matricos tikrines reikšmes ir tikrinį vektorių
-x = np.linspace(0, 10)
-dx = x[1] - x[0]
-y = x ** 2 + 1
-dydx = np.gradient(y, dx)
+def n10_matrix_eigenvector():
+    matrix = np.array([[1, 2, 3], [-3, -2, -1], [4, 2, 3]])
+    return np.linalg.eig(matrix)
 
-print(dydx)
+
+print(n10_matrix_eigenvector())
 
 
 # 11. Pasirinktos funkcijos išvestinę
-def f(x):
-    return x ** 4 + x ** 2
+def n11_diff():
+    x = sp.Symbol('x')
+    y = x ** 4 + x ** 2
+    return y.diff(x)
 
-print(derivative(f, 1.0, dx=1e-6))
+
+print(n11_diff())
+
 
 # 12. Pasirinktos funkcijos apibrėžtinį ir neapibrėžtinį integralus
-func = lambda x: x ** 4
-res = integrate.quad(func, 0, 4)
-
-print(res)
-
-x = sym.symbols('x')
-x = sym.integrate(x ** 4)
-
-print(x)
+def n12_integral():
+    x = sp.Symbol('x')
+    a = sp.Symbol('y')
+    b = sp.Symbol('z')
+    y = 6 * x + 42
+    return sp.integrate(y, (x,)), sp.integrate(y, (x, a, b))
